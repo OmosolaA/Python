c0df0cf411e40ae5dc75b29ba6561ccbde43cb04
Omosola Awoyemi <omosola.a.awoyemi@gmail.com>
2019-07-26 14:19:37 -0400

    Containers
    Practicing Containers

---
+++ containers.py
@@ -0,0 +1,33 @@
+"""
+I am learning about 3 commonly used containers: lista, tuples, and dictionary
+"""
+
+# Create a list of your favorite musicians
+
+musicians = ["Rico Nasty", "Lucky Daye", "Mac Miller", "Solange"]
+print(musicians)
+
+# Create a list of tuples, with each tuple containing the longitude and latitude of
+# somewhere you've lived or visited
+locations = ("Townley Apartments", "23V2+V8 Beltsville, District 1, Vansville, MD")
+print(locations)
+
+# Create a dictionary that contains different attributes about you: height,
+# favorite color, favorite author, etc.
+Bola = {"Height":
+        "5'4",
+        "Favorite Color":
+        "Yellow",
+        "Favorite Animal":
+        "Giraffe"
+        }
+print(Bola)
+
+# Write a program that lets the user ask yout height, favorite color, or author,
+# and returns the result from the dictionary you created in the previous challenge
+
+# Create a dictionary mapping your favorite musicians to a list of your favorite
+# songs by them
+
+#Lists, tuples, and dictionaries are just a few of the containers built into Python
+#. Research Python sets(a type of container). When would you use a set?
